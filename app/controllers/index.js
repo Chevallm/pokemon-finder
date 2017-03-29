45d87777384ec1ed8199a70165bcf30ab712a9fc
Maxime Chevallier-Pichon <maxime.chevallier-pichon@pole-emploi.fr>
2017-03-29 10:58:45 +0200

    fonc ok

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     filter:null,
+    pokemonselect:null,
     filterlist:Ember.computed('filter',function(){
       if(Ember.isEmpty(this.get('filter'))){
-        return this.get('model');
+        return this.get('model').Pokedex.Pokemon;
       }
       var lesPokemonsCorrespondants = [];
       var lesPokemons = this.get('model').Pokedex.Pokemon;
       lesPokemonsCorrespondants = lesPokemons.filter(pokemon => {
-        return (pokemon.Nom.search(this.get('filter')) > -1)
+        return (pokemon.Nom.toLowerCase().search(this.get('filter')) > -1); 
       });
+      return lesPokemonsCorrespondants;
     })
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,11 @@ export default Ember.Route.extend({
   model() {
     return Ember.$.getJSON('pokemons.json');
   }
-/*filterList:Ember.computed('filter',function(){
-  if(Ember.isEmpty(this.get('filter'))){
-    return this.get('model');
-  }
-})*/
+  /*
+  filterList:Ember.computed('filter', function(){
+    if(Ember.isEmpty(this.get('filter'))){
+      return this.get('model');
+    }
+  })
+  */
 });
