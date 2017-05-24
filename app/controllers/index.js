f27bd4bed98c72448789614111ef77e57e14a470
Maxime Chevallier-Pichon <maxime.chevallier-pichon@pole-emploi.fr>
2017-05-24 16:48:00 +0200

    add favicon

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    filter:null,
-    pokemonselect:null,
-    filterlist:Ember.computed('filter',function(){
-      if(Ember.isEmpty(this.get('filter'))){
-        return this.get('model').Pokedex.Pokemon;
-      }
-      var lesPokemonsCorrespondants = [];
-      var lesPokemons = this.get('model').Pokedex.Pokemon;
-      lesPokemonsCorrespondants = lesPokemons.filter(pokemon => {
-        return (pokemon.Nom.toLowerCase().search(this.get('filter').toLowerCase()) > -1); 
-      });
-      return lesPokemonsCorrespondants;
-    })
+  filter: null,
+  pokemonselect: null,
+  filterlist: Ember.computed('filter', function() {
+    if (Ember.isEmpty(this.get('filter'))) {
+      return this.get('model').Pokedex.Pokemon;
+    }
+    var lesPokemonsCorrespondants = [];
+    var lesPokemons = this.get('model').Pokedex.Pokemon;
+    lesPokemonsCorrespondants = lesPokemons.filter(pokemon => {
+      return (pokemon.Nom.toLowerCase().search(this.get('filter').toLowerCase()) > -1);
+    });
+    return lesPokemonsCorrespondants;
+  })
 
 });
